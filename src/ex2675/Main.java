370f2cbb3bac9ca0c18883a3b8e14a3cef4ca013
= <gudrnr2004@naver.com>
2020-12-31 16:56:22 +0900

    update

---
+++ src/ex2675/Main.java
@@ -0,0 +1,23 @@
+package ex2675;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for(int i = 0; i < T; i++) {
+	
+			int R = sc.nextInt();
+			String S = sc.next();	// nextLine() 을 쓰면 error!
+			
+			for(int j = 0; j < S.length(); j++) {           
+				for(int k = 0; k < R; k++) {	// R 만큼 반복 출력
+					System.out.print(S.charAt(j));
+				}
+			}
+			System.out.println();
+		}
+	}
+}
