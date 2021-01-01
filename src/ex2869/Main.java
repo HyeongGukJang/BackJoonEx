9d812ccd16b704460cb92dda1659fcabc2022b7f
= <gudrnr2004@naver.com>
2021-01-01 19:04:36 +0900

    .

---
+++ src/ex2869/Main.java
@@ -0,0 +1,26 @@
+package ex2869;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		long a = sc.nextLong(); // 낮에 올라갈 수 있는 높이
+		long b = sc.nextLong(); // 밤에 미끄러져 내려오는 높이
+		long v = sc.nextLong(); // 총 높이.
+
+		if (a >= v) {
+			System.out.println(1);
+		} else {
+			v -= a;
+			long key = a - b;
+			long ans = v / key;
+			System.out.println(ans);
+			if (v % key != 0) {
+				ans++;
+			}
+			System.out.println(ans + 1);
+		}
+	}
+}
